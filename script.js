21289db09126aa80c728cfcb38b19511d4f8837f
Vallabhu Narayana Murthy <97281791+vnmweb@users.noreply.github.com>
2025-08-30 01:36:17 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  const path = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.nav-links a').forEach(a=>{
-    if(a.getAttribute('href')===path) a.classList.add('active');
-  });
-})();
